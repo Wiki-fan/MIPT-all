65fcbe0b560cd465d6551fea3aefbe892207e928
wiki-fan <uberslowpoke@gmail.com>
2016-04-07 16:41:30 +0300

    s6 fixed, correct overriding, throw_sig removed

--- task-s6-print_sig/print_sig.c
+++ task-s6-print_sig/print_sig.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200901L
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,11 +23,11 @@ int main()
     sigemptyset( &sa.sa_mask );
     for( i = 1; i < 32; ++i )
     {
-        if( sigismember( &set, i ))   /* If signal i really exists */
+        if(i != SIGKILL && i != SIGSTOP)   /* If signal i can be overriden */
         {
             if( sigaction( i, &sa, NULL) == -1 )
             {
-                /*err( 3, "Unable to set handler" );*/
+                err( 20, "Unable to set handler" );
             }
         }
     }
@@ -36,7 +37,6 @@ int main()
         sigwaitinfo( &set, &info );
         printf( "%d\n", info.si_pid );
     }
-
 
     return 0;
 }
