573c706be91a9cf5e16a1cba8a9418611346336a
wiki-fan <uberslowpoke@gmail.com>
2016-05-01 15:24:35 +0300

    s5 getopt finished

--- task-s5-l2_getopt/main.c
+++ task-s5-l2_getopt/main.c
@@ -3,92 +3,91 @@
 #include <err.h>
 #include <getopt.h>
 #include <stdlib.h>
-#include "../task-l1-nucleotide_pack/nucleotide_pack.h"
+#define PACK_NUCLEOTIDS 0
+#define UNPACK_NUCLEOTIDS 1
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
-    char mode;
-    char* in, *out;
-    int optind = 0;
+	char mode;
+	int mode_optind = -1; /* Position of mode in options */
+	char* in, * out;
+	int optind = 0;
 
-    int c;
+	int c;
 
-    do
-    {
-        int curind = optind;
-        static struct option long_options[] =
-        {
-            { "help",    no_argument,       0, 'h' },
-            { "version", no_argument,       0, 'v' },
-            { "pack",    required_argument, 0, 'p' },
-            { "unpack",  required_argument, 0, 'u' },
-            { 0, 0,                         0, 0 }
-        };
-        c = getopt_long(argc, argv, "hv",
-                        long_options, &optind);
+	do {
+		static struct option long_options[] =
+				{
+						{ "help",    no_argument,       0, 'h' },
+						{ "version", no_argument,       0, 'v' },
+						{ "pack",    required_argument, 0, 'p' },
+						{ "unpack",  required_argument, 0, 'u' },
+						{ 0, 0,                         0, 0 }
+				};
+		c = getopt_long( argc, argv, "hv", long_options, &optind );
 
-        switch (c)
-        {
-        case 0:
-            /* If this option set a flag, do nothing else now. */
-            if (long_options[optind].flag != 0)
-                break;
-            printf ("option %s", long_options[optind].name);
-            if (optarg)
-                printf (" with arg %s", optarg);
-            printf ("\n");
-            break;
+		switch( c ) {
+			case 'h':
+				puts( "Usage:\n./nucleotide_pack --[pack/unpack] in_file out_file\n./nucleotide_pack -h\n./nucleotide_pack -v" );
+				exit( 0 );
+			case 'v':
+				puts( "Version 0.1" );
+				exit( 0 );
+			case 'p':
+				if( mode_optind != -1 ) {
+					errx( 21, "You can't specify both modes simultaneously" );
+				}
+				mode = PACK_NUCLEOTIDS;
+				mode_optind = optind;
+				optind++;
+				if( optind < argc && *argv[optind] != '-' ) {
+					in = optarg;
+					out = argv[optind];
+				}
+				else {
+					errx( 20, "--pack option requires TWO arguments" );
+				}
+				break;
 
-        case 'h':
-            puts ("Usage:\n./nucleotide_pack [pack/unpack] in_file out_file");
-            exit(0);
-        case 'v':
-            puts ("Version 0.1");
-            exit(0);
-        case 'p':
-            mode =  PACK_NUCLEOTIDS;
-            optind++;
-            if (optind < argc && *argv[optind] != '-')
-            {
-                in = optarg;
-                out = argv[optind];
-            }
-            else
-            {
-                errx(20, "--pack option requires TWO arguments");
-            }
-            break;
+			case 'u':
+				if( mode_optind != -1 ) {
+					errx( 21, "You can't specify both modes simultaneously" );
+				}
+				mode = UNPACK_NUCLEOTIDS;
+				mode_optind = optind;
+				optind++;
+				if( optind < argc && *argv[optind] != '-' ) {
+					in = optarg;
+					out = argv[optind];
+				}
+				else {
+					errx( 20, "--unpack option requires TWO arguments" );
+				}
+				printf( "Unpacking nucleotids from %s to %s\n", in, out );
+				printf( "Unpacked successful\n" );
+				break;
 
-        case 'u':
-            mode =  UNPACK_NUCLEOTIDS;
-            optind++;
-            if (optind < argc && *argv[optind] != '-')
-            {
-                in = optarg;
-                out = argv[optind];
-            }
-            else
-            {
-                errx(20, "--unpack option requires TWO arguments");
-            }
-            break;
+			case '?':
+				/* getopt_long writes error by itself */
+				errx( 11, "Unknown parameter" );
 
-        case '?':
-            /* getopt_longs writes error by itself */
-            /*if (optopt)  printf("Bad short opt '%c'\n", optopt);
-            else  printf("Bad long opt \"%s\"\n", argv[curind]);*/
-            errx(11, "Unknown parameter");
+			case -1:
+				break;
 
-        case -1:
-            break;
+			default:
+				err( 3, "Unreachable code" );
+		}
+	}
+	while( c != -1 );
 
-        default:
-            err(3, "Unreachable code");
-        }
-    }
-    while (c != -1);
+	if( optind == 0 ) {
+		printf( "Nucleotide packing.\nFor help see ./nucleotide_pack -h\n" );
+		return 0;
+	}
+	printf( "%sacking nucleotids from %s to %s\n", mode == PACK_NUCLEOTIDS ? "P" : "Unp", in, out );
+	/*nucleotide_pack(mode, in, out);*/
+	printf( "%sacked successful\n", mode == PACK_NUCLEOTIDS ? "P" : "Unp" );
 
-    nucleotide_pack(mode, in, out);
 
-    return 0;
+	return 0;
 }
