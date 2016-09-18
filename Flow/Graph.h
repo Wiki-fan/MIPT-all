ddcffabd246dcc42ec9fd3f3592d1251df021ace
sharik <ph134@yandex.ru>
2016-09-18 22:15:58 +0300

    Graph interface parts

---
+++ Flow/Graph.h
@@ -0,0 +1,102 @@
+#pragma once
+#include "../stdafx.h"
+#include "../Other/Smth.h"
+#include "IGraph.h"
+
+//template<template<typename> typename template<typename> typename AT>
+template<typename vtype, typename etype, typename VST, typename EST>
+class Graph : public IGraph<vtype, etype, VST, EST> {
+
+public:
+    Graph() = default;
+    ~Graph() = default;
+
+    //Граф с size вершинами.
+    Graph(size_t size) {
+        vertices.resize(size);
+    }
+
+    // Добавить ребро u -> v.
+    void insertEdge(size_t u, size_t v, EST data) {
+        insertEdgeLocal(u, v, data);
+        insertEdgeLocal(v, u, data);
+    }
+
+    // Получить значение вершины с номером vNum.
+    VST GetVertexVal(vtype vNum) {
+        return vertices[vNum].val;
+    }
+
+    // Получить количество вершин.
+    size_t GetSize() const { return vertices.size(); }
+
+    GraphEdgeIterator begin(vtype v) { return GraphEdgeIterator(*this, v, true); }
+    GraphEdgeIterator end() { return GraphEdgeIterator(*this, 0, false); }
+
+private:
+    struct GraphEdge : public IGraph::Edge {
+        vtype u, v;
+        etype prev = NullEdge, next = NullEdge;
+        GraphEdge(vtype u, vtype v, EST data)
+                : u(u), v(v), stored(data) {}
+    };
+
+    static const vtype NullEdge = -1;
+    std::vector<GraphEdge> edges;
+    std::vector<Vertex> vertices;
+
+    void insertEdgeLocal(vtype u, vtype v, EST data) {
+        edges.push_back(GraphEdge(u, v, data));
+        // Set first if necessary.
+        if (vertices[u].first == NullEdge) {
+            vertices[u].backup_first = vertices[u].first = edges.size() - 1;
+        } else {
+            etype tmp = vertices[u].last;
+            // Set previous and next.
+            edges[tmp].next = edges.size() - 1;
+            edges.back().prev = tmp;
+        }
+        // Set last.
+        vertices[u].last = edges.size() - 1;
+    }
+
+    class GraphEdgeIterator : public std::iterator<std::forward_iterator_tag, Edge> {
+    public:
+        GraphEdgeIterator(Graph& graph_, vtype v_, bool first) {
+            graph = &graph_;
+            if (first) {
+                v = v_;
+                e = graph->vertices[v].first;
+                edge = &graph->edges[e];
+            } else {
+                e = graph->NullEdge;
+            }
+        }
+
+        bool hasNext() { return edge->next != graph->NullEdge; }
+        //bool hasPrev() { return edge->prev != graph->NullEdge; }
+
+        bool operator==(const GraphEdgeIterator& other) const { return graph == other.graph && e == other.e; }
+        bool operator!=(const GraphEdgeIterator& other) const { return !(*this == other); }
+
+        GraphEdgeIterator& operator++() {
+            if (!hasNext()) {
+                e = graph->NullEdge;
+            } else {
+                e = edge->next;
+                edge = &graph->edges[e];
+            }
+            return *this;
+        }
+
+    private:
+        vtype v; // Vertex that iterator belongs to.
+        etype e;
+        Graph* graph;
+        GraphEdge* edge;
+
+        friend class Network;
+    };
+};
+
+
---
+++ Flow/IGraph.h
@@ -0,0 +1,87 @@
+#pragma once
+#include "../stdafx.h"
+
+//template<template<typename> typename template<typename> typename AT>
+template<typename etype, typename vtype, typename VST, typename EST>
+class IGraph {
+    // Forward declaration.
+    struct Vertex;
+    struct Edge;
+public:
+    IGraph() = default;
+    virtual ~IGraph() = default;
+
+    //Граф с size вершинами.
+    virtual IGraph(size_t size) = 0;
+
+    // Добавить ребро u -> v.
+    virtual void insertEdge(vtype u, vtype v, EST weight) = 0;
+
+    /*// Добавить вершину со значением val. Возвращает номер добавленной вершины.
+    size_t AddVertex( VT val )
+    {
+        vertices.push_back( Vertex( val ));
+        return vertices.size()-1;
+    }*/
+
+    /*// Получить контейнер рёбер, входящих в вершину.
+    const AT<Edge>& GetIncomingEdges( size_t vNum ) const
+    {
+        return vertices[vNum].in;
+    }
+
+    // Получить контейнер рёбер, выходящих из вершины.
+    const AT<Edge>& GetOutcomingEdges( size_t vNum ) const
+    {
+        return vertices[vNum].out;
+    }*/
+
+    /*// Получить значение вершины с номером vNum.
+    VST GetVertexVal( size_t vNum )
+    {
+        return vertices[vNum].val;
+    }*/
+
+    // Получить количество вершин.
+    virtual size_t GetSize() = 0;
+
+protected:
+    struct Edge {
+        EST stored;
+        virtual vtype getV() = 0;
+        virtual vtype getU() = 0;
+        virtual vtype getStored() { return stored; }
+    };
+
+    struct Vertex {
+        VST val;
+        Vertex() : val(0) {}
+        explicit Vertex(int _val) : val(_val) {}
+        ~Vertex() {}
+        virtual vtype getStored() = 0;
+    };
+
+    //std::vector<Vertex> vertices;
+
+    class IGraphEdgeIterator : public std::iterator<std::forward_iterator_tag, Edge> {
+    public:
+        virtual IGraphEdgeIterator(IGraph& IGraph_, vtype v_, bool first) = 0;
+
+        virtual bool hasNext() = 0;
+        //bool hasPrev() { return edge->prev != igraph->NullEdge; }
+
+        virtual bool operator==(const IGraphEdgeIterator& other) const = 0;
+        virtual bool operator!=(const IGraphEdgeIterator& other) const = 0;
+
+        virtual IGraphEdgeIterator& operator++() = 0;
+
+    private:
+        /*vtype v; // Vertex that iterator belongs to.
+        etype e;
+        IGraph* igraph;
+        Edge* edge;*/
+
+        friend class IGraph;
+    };
+};
+
