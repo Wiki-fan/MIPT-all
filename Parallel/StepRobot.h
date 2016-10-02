d6ba09cd50f18dc1fba28af87e20937ac6d635cd
wiki-fan <uberslowpoke@gmail.com>
2016-10-02 16:13:14 +0300

    Parallels task finished

--- Parallel/StepRobot.h
+++ Parallel/StepRobot.h
@@ -37,7 +37,7 @@ private:
 
 class RobotWithSemaphores {
 public:
-    RobotWithSemaphores():sem1(-1), sem2(-1) {};
+    RobotWithSemaphores():sem1(1), sem2(0) {};
 
     void start() {
         std::thread t0(&RobotWithSemaphores::step, this, 0);
@@ -48,18 +48,26 @@ public:
 
 private:
     Semaphore sem1, sem2;
-    bool current = 0;
     void step(int leg) {
-        for (int i = 0; i < 1000; ++i) {
-            if(leg == current) {
+        if(leg == 0) {
+            for (int i = 0; i < 1000; ++i) {
                 sem1.wait();
-                std::cout << (leg == 0 ? "left" : "right") << std::endl;
+                std::cout << "left" << std::endl;
+                std::cout.flush();
+                sem2.post();
+                sem1.wait();
+                sem1.post();
+            }
+            sem2.post();
+        } else if (leg == 1) {
+            for (int i = 0; i < 1000; ++i) {
+                sem2.wait();
+                std::cout << "right" << std::endl;
                 std::cout.flush();
                 sem1.post();
                 sem2.wait();
                 sem2.post();
             }
         }
-
     }
 };
