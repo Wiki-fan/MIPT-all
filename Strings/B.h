a5cde66423f29334ab1d794ca6aa637869f0fe24
wiki-fan <uberslowpoke@gmail.com>
2016-12-04 17:06:10 +0300

    Reverse removed

--- Strings/B.h
+++ Strings/B.h
@@ -12,7 +12,6 @@ public:
         std::cin >> s;
         int n = s.length();
         std::reverse(s.begin(), s.end());
-        //std::vector<int> zf = std::move(computeZFunction(s));\
         // inline
         std::vector<int> zf(n);
         int left = 0, right = 0;
@@ -26,14 +25,11 @@ public:
                 right = i + zf[i];
             }
         }
-        //std::vector<int> pf = std::move(computePrefixFunction(s));
-        std::reverse(s.begin(), s.end());
 
         /*for (int i = 0; i < n; ++i) {
-            std::cout << zf[i] << ' ' << pf[i] << '\n';
+            std::cout << zf[i] <<'\n';
         }*/
 
-        int ansPos = -1;
         int pos = 1;
         // Инициализация так, чтобы вся дробная часть вывелась как период
         int ansLen = n;
@@ -47,49 +43,19 @@ public:
                 int curLen = n - zf[pos];
                 if (ansLen > curLen) {
                     ansLen = curLen;
-                    start = n - zf[pos] - pos;
+                    start = zf[pos] + pos;
                 }
                 ++pos;
             }
         }
 
-        for (int i = 0; i < start; ++i) {
+        for (int i = n - 1; i >= start; --i) {
             std::cout << s[i];
         }
         std::cout << '(';
-        for (int i = start; i < ansLen; ++i) {
+        for (int i = start - 1; i >= n - ansLen; --i) {
             std::cout << s[i];
         }
         std::cout << ")\n";
-    }
-
-    std::vector<int> computeZFunction(const std::string& s) {
-        int n = s.length();
-        std::vector<int> zf(n);
-        int left = 0, right = 0;
-        for (int i = 0; i < n; ++i) {
-            zf[i] = std::max(0, std::min(right - i, zf[i - left]));
-            while (i + zf[i] < n && s[zf[i]] == s[i + zf[i]]) {
-                ++zf[i];
-            }
-            if (i + zf[i] >= right) {
-                left = i;
-                right = i + zf[i];
-            }
-        }
-        return zf;
-    }
-
-    std::vector<int> computePrefixFunction(const std::string& s) {
-        int n = s.length();
-        std::vector<int> pf(n);
-        for (int i = 0; i < n; ++i) {
-            for (int k = 0; k <= i; ++k) {
-                if (s.substr(0, k) == s.substr(i - k + 1, k)) {
-                    pf[i] = k;
-                }
-            }
-        }
-        return pf;
     }
 };
