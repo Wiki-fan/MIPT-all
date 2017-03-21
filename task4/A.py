48edb9b914bc6fcbbeeda1f3937e261c04d9e677
wiki-fan <uberslowpoke@gmail.com>
2017-03-21 22:23:00 +0300

    task 4 A final version

--- task4/A.py
+++ task4/A.py
@@ -3,17 +3,6 @@ import argparse
 import re
 
 
-def is_punctuation_marks(s):
-    for c in s:
-        if c not in {",", ".", "?", "!", "-", ":", "'"}:
-            return False
-    return True
-
-
-def is_single_piece(s):
-    return s != ' ' and not is_punctuation_marks(s)
-
-
 def get_paragraphs(inp):
     lines = inp.readlines()
     paragraphs = []
@@ -30,26 +19,35 @@ def get_paragraphs(inp):
 
 
 def process_paragraph(line, paragraph_spaces, line_length):
-    arr = re.findall(r'[A-Za-z0-9]+|[,.?!\-:\']', line)
+    def is_punctuation_marks(s):
+        for c in s:
+            if c not in {",", ".", "?", "!", "-", ":", "'"}:
+                return False
+        return True
+
+    def is_single_piece(s):
+        return s != ' ' and not is_punctuation_marks(s)
+
+    words = re.findall(r'[A-Za-z0-9]+|[,.?!\-:\']', line)
 
     new_arr = ['']
-    for word in arr:
+    for word in words:
         if is_punctuation_marks(word):
             new_arr[-1] += word
         else:
             new_arr.append(word)
-    arr = new_arr
+    words = new_arr
 
-    out = ''
+    out_text = ''
     out_line = ' ' * paragraph_spaces
     cur_len = paragraph_spaces
 
-    for word in arr:
+    for word in words:
         if len(word) > line_length:
             raise Exception('Too long')
         if cur_len + len(word) >= line_length:
-            out += out_line
-            out += '\n'
+            out_text += out_line
+            out_text += '\n'
             out_line = word
             cur_len = len(word)
         else:
@@ -59,37 +57,40 @@ def process_paragraph(line, paragraph_spaces, line_length):
             cur_len += len(word)
             out_line += word
 
-    out += out_line
-    out += '\n'
+    out_text += out_line
+    out_text += '\n'
 
-    return out
+    return out_text
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Форматирователь текста.')
+    parser = argparse.ArgumentParser(description='Text formatter')
     parser.add_argument('--input', '-i', metavar='filename', type=str,
                         default=None)
     parser.add_argument('--output', '-o', metavar='filename', type=str,
                         default=None)
-    parser.add_argument('--line-length', '-l', metavar='w', type=int)
-    parser.add_argument('--paragraph-spaces', '-p', metavar='b', type=int)
+    parser.add_argument('--line-length', '-l', metavar='w', type=int,
+                        required=True)
+    parser.add_argument('--paragraph-spaces', '-p', metavar='b', type=int,
+                        required=True)
 
     args = parser.parse_args()
 
     if args.paragraph_spaces > args.line_length:
         raise Exception("Paragraph spaces greater than line length")
 
-    inp = sys.stdin if args.input is None else open(args.input, 'r')
-    out = sys.stdout if args.output is None else open(args.output, 'w')
-    paragraphs = get_paragraphs(inp)
-    for par in paragraphs:
-        to_append = process_paragraph(par,
+    input_stream = sys.stdin if args.input is None else open(args.input, 'r')
+    output_stream = sys.stdout if args.output is None else open(args.output,
+                                                                'w')
+    paragraphs = get_paragraphs(input_stream)
+    for paragraph in paragraphs:
+        to_append = process_paragraph(paragraph,
                                       args.paragraph_spaces,
                                       args.line_length)
-        out.write(to_append)
+        output_stream.write(to_append)
 
-    inp.close()
-    out.close()
+    input_stream.close()
+    output_stream.close()
 
 
 main()
